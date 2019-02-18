63704fa52261bedff398f4141b941435918af722
Jonathan Vilella <vilellaj@hotmail.com>
2019-02-18 20:05:28 -0300

    Fix

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/api/posts', async (req, res) => {
         const posts = await Post.find();
         res.json(posts);
     }
-    catch(error) {
+    catch (error) {
         res.status(500).send(error);
     }
 });
@@ -25,7 +25,7 @@ app.get('/api/posts/:id', async (req, res) => {
         const post = await Post.findById(req.params.id);
         res.json(post);
     }
-    catch(error) {
+    catch (error) {
         res.status(500).send(error);
     }
 });
@@ -43,7 +43,7 @@ app.post('/api/posts', async (req, res) => {
 
         await post.save();
 
-        res.status(201).json({ post });
+        res.status(201).json(post);
     } catch (error) {
         res.status(500).send(error);
     }
@@ -65,7 +65,7 @@ app.post('/api/posts/:id/comments', async (req, res) => {
 
 app.get('/api/posts/user/:id', async (req, res) => {
     try {
-        const posts = await Post.find({userId: req.params.id});
+        const posts = await Post.find({ userId: req.params.id });
         res.status(200).json(posts);
     } catch (error) {
         res.status(500).send(error);
@@ -74,19 +74,26 @@ app.get('/api/posts/user/:id', async (req, res) => {
 
 app.put('/api/posts/:id', async (req, res) => {
     try {
-        const post = await Post.find({postId: req.params.id});
-        Object.assign(post, req.body);
-        await post.save();
-        res.status(200).json(post);
+        const post = await Post.findOne({ _id: req.params.id });
+
+        if (post) {
+            Object.assign(post, req.body);
+            await post.save();
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({message: 'Post not found'});
+        }
+
     } catch (error) {
+        console.dir(error);
         res.status(500).send(error);
     }
 });
 
 app.delete('/api/posts/:id', async (req, res) => {
     try {
-        await Post.deleteOne({_id: req.params.id});
-        res.status(200).json({"message": "Post deleted"});
+        await Post.deleteOne({ _id: req.params.id });
+        res.status(200).json({ "message": "Post deleted" });
     } catch (error) {
         res.status(500).send(error);
     }
